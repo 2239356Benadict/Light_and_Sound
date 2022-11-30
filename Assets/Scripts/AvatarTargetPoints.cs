74f5a2787ef49776f955fea00b65715f11c1acb5
Benadict <115377654+2239356Benadict@users.noreply.github.com>
2022-11-30 12:17:14 +0000

    Implemented Nav mesh agent
    The Nav mesh agent has been implemented on one avatar and working properly along with walking animation. The time of player movement and destination point checking with the current avatar position has to be done next.

--- Assets/Scripts/AvatarTargetPoints.cs
+++ Assets/Scripts/AvatarTargetPoints.cs
@@ -1,27 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AvatarTargetPoints : MonoBehaviour
 {
-    public GameObject[] targetPoints;
+    public GameObject[] avatarToMove; // Avatar gameobjects
+    public GameObject[] targetPoints; // Target points for avatar to move
     public Animator animator;
+    public bool moveToTarget;
+    public float moveToTargetSpeed;
+
+    public NavMeshAgent meshAgent;
 
     private void Start()
     {
-        InvokeRepeating("AvatarWalking", 1f, 20f);
+        //InvokeRepeating("AvatarWalking", 1f, 20f);
+        meshAgent = GetComponent<NavMeshAgent>();
+       
     }
     void Update()
     {
-        
+        if (moveToTarget)
+        {
+            AvatarWalking();
+        }
     }
 
+    /// <summary>
+    /// Method to move the avatar to the target points
+    /// </summary>
     public void AvatarWalking()
     {
         GameObject currentTarget = targetPoints[Random.Range(0, 7)];
         Debug.Log(currentTarget.name);
-        this.gameObject.transform.LookAt(currentTarget.transform.position);
-        
-        animator.Play("Walking");
+        meshAgent.destination = currentTarget.transform.position;
+        if(gameObject.transform.position != currentTarget.transform.position)
+        {
+            gameObject.transform.LookAt(currentTarget.transform);
+            meshAgent.speed = moveToTargetSpeed;
+            //gameObject.transform.Translate(Vector3.forward * moveToTargetSpeed * Time.deltaTime);
+            animator.Play("Walking");
+
+        }
+        else if(gameObject.transform.position == currentTarget.transform.position)
+        {
+            animator.Play("Idle");
+        }
     }
 }
